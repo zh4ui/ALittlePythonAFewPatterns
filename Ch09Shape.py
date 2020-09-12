13934c58992aafc792fad46d9aba2aadb5554f2d
zh4ui <zh4ui@users.noreply.github.com>
2020-09-12 23:59:51 +0800

    stop at 150

--- Ch09Shape.py
+++ Ch09Shape.py
@@ -1,10 +1,21 @@
+"""
+>>> Circle(10).accept(HasPt(CartesianPt(10,10)))
+False
+>>> Square(10).accept(HasPt(CartesianPt(10,10)))
+True
+>>> Trans(CartesianPt(5,6), Circle(10)).accept(HasPt(CartesianPt(10,10)))
+True
+
+>>> a = Trans(CartesianPt(12,2), Union(Square(10), Trans(CartesianPt(4,4), Circle(5))))
+"""
+
 from abc import ABC, abstractmethod
 from Ch09Point import *
 
 
 class Shape(ABC):
     @abstractmethod
-    def accept(self, ask: "ShapeVisitor") -> bool:
+    def accept(self, ask) -> bool:
         pass
 
 
@@ -16,12 +27,30 @@ class Circle(Shape):
         return ask.forCircle(self.radius)
 
 
-class Sqaure(Shape):
-    def __init__(self, size: int):
-        self.size = size
+class Square(Shape):
+    def __init__(self, side: int):
+        self.side = side
 
     def accept(self, ask):
-        return ask.forSqaure(self.size)
+        return ask.forSqaure(self.side)
+
+
+class Trans(Shape):
+    def __init__(self, q: Point, s: Shape):
+        self.q = q
+        self.s = s
+
+    def accept(self, ask):
+        return ask.forTrans(self.q, self.s)
+
+
+class Union(Shape):
+    def __init__(self, s: Shape, t: Shape):
+        self.s = s
+        self.t = t
+
+    def accept(self, ask):
+        return ask.forUnion(self.s, self.t)
 
 
 class ShapeVisitor(ABC):
@@ -30,12 +59,32 @@ class ShapeVisitor(ABC):
         pass
 
     @abstractmethod
-    def forSqaure(self, size: int) -> bool:
+    def forSqaure(self, side: int) -> bool:
         pass
 
     @abstractmethod
     def forTrans(self, q: Point, s: Shape) -> bool:
         pass
+
+
+class UnionVisitor(ABC):
+    @abstractmethod
+    def forUnion(self, s: Shape, t: Shape) -> bool:
+        pass
+
+
+class HasPt(ShapeVisitor):
+    def __init__(self, p: Point):
+        self.p = p
+
+    def forCircle(self, radius: int) -> bool:
+        return self.p.distanceToOrigin() <= radius
+
+    def forSqaure(self, side: int) -> bool:
+        return self.p.x <= side and self.p.y <= side
+
+    def forTrans(self, q: Point, s: Shape) -> bool:
+        return s.accept(HasPt(self.p.minus(q)))
 
 
 if __name__ == "__main__":
